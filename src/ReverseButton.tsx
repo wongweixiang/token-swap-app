e0c8b6f87da680bb6d54007fc89976025e054ace
Wei Xiang <wongweixiangjulian@gmail.com>
2025-07-29 11:54:05 +0800

    fix: reverse button

--- src/ReverseButton.tsx
+++ src/ReverseButton.tsx
@@ -1,15 +1,25 @@
-import swapVertical from "./assets/swap-vertical.svg";
 import type { FC } from "react";
+import { useSelectedTokens } from "./SelectedTokensContext";
+import { cn } from "@/lib/utils";
+import swapVertical from "./assets/swap-vertical.svg";
 
 type ReverseButtonProps = {
   onClick: () => void;
 };
 
 export const ReverseButton: FC<ReverseButtonProps> = ({ onClick }) => {
+  const { fromToken, toToken } = useSelectedTokens();
+
+  const isEnabled = fromToken && toToken;
+
   return (
     <button
       type="button"
-      className="bg-emerald-200 p-2 rounded-4xl cursor-pointer"
+      disabled={!isEnabled}
+      className={cn(
+        "p-2 rounded-4xl",
+        isEnabled ? "bg-emerald-200 cursor-pointer" : "bg-emerald-50"
+      )}
       onClick={onClick}
     >
       <img src={swapVertical} alt="Swap Icon" className="w-6 h-6" />
